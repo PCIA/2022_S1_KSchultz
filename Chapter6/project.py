39e1151dcb0596b7d296fcf6b8c42496c1ecbf94
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2022-11-21 14:10:51 -0800

    small changes

--- Chapter6/project.py
+++ Chapter6/project.py
@@ -1,3 +1,27 @@
-myName = ""
-myName = input("What is your name?")
-myLunch = ""
+import math
+x = input("Give a number between 10 and 20")
+y = math.sqrt(x)
+if x < 10 and x > 20:
+    print("y")
+else:
+    print("wrong number")
+
+#import math
+
+#create variable to store given number, myNum
+
+#ask user for number between 10 and 20
+
+#store variable in myNum
+
+#value of sqrt=math.sqrt(myNum)
+
+#if myNum >10,<20:
+
+        #print(value of sqrt)
+
+#else:
+
+        #print("number is not between 10 and 20")
+
+     
--- whattheheck.py
+++ whattheheck.py
@@ -1,4 +1,9 @@
-import pygame.examples.aliens
-
-# source: https://github.com/takluyver/pygame/blob/master/examples/aliens.py
-pygame.examples.aliens.main()
+import math
+x = input("Give a number between 10 and 20: ")
+answer = math.sqrt(x)
+if myVar < 10:
+    print("That's too small!")
+elif myVar > 20:
+    print("That's too big!")
+else:
+    print(str(answer))
