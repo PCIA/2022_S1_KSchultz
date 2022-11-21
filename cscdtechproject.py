4a2f354f470b15460269027c1963c78bd65ae212
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2022-11-21 14:09:40 -0800

    small changes

--- cscdtechproject.py
+++ cscdtechproject.py
@@ -9,17 +9,17 @@ paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=10, stretch_len=6)
+paddle_a.shapesize(stretch_wid=6, stretch_len=2)
 paddle_a.penup()
-paddle_a.goto(-600, 0)
+paddle_a.goto(-400, 0)
 
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
 paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=10, stretch_len=6)
+paddle_b.shapesize(stretch_wid=6, stretch_len=2)
 paddle_b.penup()
-paddle_b.goto(600, 0)
+paddle_b.goto(400, 0)
 
 pong_ball = turtle.Turtle()
 pong_ball.speed(40)
@@ -29,7 +29,7 @@ pong_ball.dx = 5
 pong_ball.dy = -5
 pong_ball.penup()
 pong_ball.goto(0, 0)
-pong_ball.shapesize(stretch_wid=3, stretch_len=3)
+#pong_ball.shapesize(stretch_wid=3, stretch_len=3)
 
 player_one = 0
 player_two = 0
@@ -70,8 +70,43 @@ sc.onkeypress(paddlebup, "Up")
 sc.onkeypress(paddlebdown, "Down")
 
 while True:
-    sc.update
+    sc.update()
 
-    pong_ball.setx()
+    pong_ball.setx(pong_ball.xcor()+pong_ball.dx)
+    pong_ball.sety(pong_ball.ycor()+pong_ball.dy)
 
-sc.mainloop()
+    if pong_ball.ycor() > 280:
+            pong_ball.sety(280)
+            pong_ball.dy *= -1
+
+    if pong_ball.ycor() < -280:
+            pong_ball.setx(280)
+            pong_ball.dx *= -1
+
+    if pong_ball.xcor() > 500:
+            pong_ball.goto(0, 0)
+            pong_ball.dy *= -1
+            player_one += 1
+            sketch.clear()
+            sketch.write("player_one : {} player_two: {}".format(
+                        player_one, player_two), align="center",
+                        font=("arial", 24, "normal"))
+
+    if pong_ball.xcor() < -500:
+            pong_ball.goto(0, 0)
+            pong_ball.dy *= -1
+            player_two += 1
+            sketch.clear()
+            sketch.write("player_one : {} player_two: {}".format(
+                                    player_one, player_two), align="center",
+                                    font=("arial", 24, "normal"))
+
+    if (pong_ball.xcor() > 360 and pong_ball.xcor() < 370) and (pong_ball.ycor() < paddle_b.ycor()+40 and pong_ball.ycor() > paddle_b.ycor()-40):
+            pong_ball.setx(360)
+            pong_ball.dx*=-1
+
+    if (pong_ball.xcor()<-360 and pong_ball.xcor()>-370) and (pong_ball.ycor()<paddle_a.ycor()+40 and pong_ball.ycor()>paddle_a.ycor()-40):
+            pong_ball.setx(-360)
+            pong_ball.dx*=-1
+
+#sc.mainloop()
