dad120e134b1c91bf611574527a5825c9399800f
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-01-09 14:01:36 -0800

    fixed errors, focusing on randomizer

--- cscdtechproject.py
+++ cscdtechproject.py
@@ -23,15 +23,15 @@ paddle_b.penup()
 paddle_b.goto(400, 0)
 
 pong_ball = turtle.Turtle()
-pong_ball.speed(40)
+pong_ball.speed(10)
 pong_ball.shape("circle")
 pong_ball.color("red")
-pong_ball.dx = 5
-pong_ball.dy = -5
+pong_ball.movevectorx = 5
+pong_ball.movevectory = -5
 pong_ball.penup()
 pong_ball.goto(0, 0)
 #pong_ball.shapesize(stretch_wid=3, stretch_len=3), unnecessary
-#ball possibly too fast, change later
+#ball possibly too fast, change later, DONE
 
 player_one = 0
 player_two = 0
@@ -67,33 +67,34 @@ def paddlebdown():
     y += 20
     paddle_b.sety(y)
 
-#QUESTION this is where I add in key functions, but they do not work
+#QUESTION this is where I add in key functions, but they do not work, FIXED
 sc.listen()
 sc.onkeypress(paddleadown, "a")
 sc.onkeypress(paddleaup, "z")
 sc.onkeypress(paddlebdown, "Up")
 sc.onkeypress(paddlebup, "Down")
 
-while True:
-    #set variable running = True above loop, change loop to while running == True
+running = True
+while running == True:
+    #set variable running = True above loop, change loop to while running == True, DONE
     sc.update()
 
-    pong_ball.setx(pong_ball.xcor()+pong_ball.dx)
-    pong_ball.sety(pong_ball.ycor()+pong_ball.dy)
-    #change all dx and dy to movevectorx and movevectory
+    pong_ball.setx(pong_ball.xcor()+pong_ball.movevectorx)
+    pong_ball.sety(pong_ball.ycor()+pong_ball.movevectory)
+    #change all movevectorx and movevectory to movevectorx and movevectory, DONE
 
     if pong_ball.ycor() > 280:
             pong_ball.sety(280)
-            pong_ball.dy *= -1
+            pong_ball.movevectory *= -1
 
     if pong_ball.ycor() < -280:
             pong_ball.sety(-280)
-            pong_ball.dy *= -1
-            #sety (280), dy *= -1, done
+            pong_ball.movevectory *= -1
+            #sety (280), movevectory *= -1, done
 
     if pong_ball.xcor() > 500:
             pong_ball.goto(0, 0)
-            pong_ball.dy *= -1
+            pong_ball.movevectory *= -1 #won't need once ball is randomized
             player_one += 1
             sketch.clear()
             sketch.write("player_one : {} player_two: {}".format(
@@ -102,19 +103,21 @@ while True:
 
     if pong_ball.xcor() < -500:
             pong_ball.goto(0, 0)
-            pong_ball.dy *= -1
+            pong_ball.movevectory *= -1
             player_two += 1
             sketch.clear()
             sketch.write("player_one : {} player_two: {}".format(
                                     player_one, player_two), align="center",
                                     font=("arial", 24, "normal"))
+#set restart direction to randomize between 4 options, do after break, call every time I want to randomize start
 
     if (pong_ball.xcor() > 360 and pong_ball.xcor() < 370) and (pong_ball.ycor() < paddle_b.ycor()+40 and pong_ball.ycor() > paddle_b.ycor()-40):
             pong_ball.setx(360)
-            pong_ball.dx*=-1
+            pong_ball.movevectorx*=-1
 
-    if (pong_ball.xcor()<-360 and pong_ball.xcor()>-370) and (pong_ball.ycor()<paddle_a.ycor()+40 and pong_ball.ycor()>paddle_a.ycor()-40):
+    if (pong_ball.xcor()<-360 and pong_ball.xcor()> -370) and (pong_ball.ycor()<paddle_a.ycor()+40 and pong_ball.ycor()>paddle_a.ycor()-40):
             pong_ball.setx(-360)
-            pong_ball.dx*=-1
+            pong_ball.movevectorx*=-1
+#paddle is a box, test for where ball bounces from paddle based on stretch, maybe stretching in odd direction
 
 #sc.mainloop() unnecessary
